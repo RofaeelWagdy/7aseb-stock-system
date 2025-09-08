9cd19cbfed86fbc2477eaa2b471a0ce95e4c5444
Rofaeel Wagdy <rofaeelwagdy@gmail.com>
2025-09-09 02:50:15 +0300

    added constants

---
+++ src/constants/FileNames.java
@@ -0,0 +1,6 @@
+package constants;
+
+public interface FileNames {
+    String TEAMS_FILENAME = "Teams.txt";
+    String SHARES_FILENAME = "Shares.txt";
+}
---
+++ src/constants/TeamConstants.java
@@ -0,0 +1,7 @@
+package constants;
+
+public interface TeamConstants {
+    int AVAILABLE_STOCK_QUANTITY = 100;
+    long STOCK_PRICE = 10;
+    long BALANCE = 1000;
+}
