492de042b0dece27ba7db4c15263082b16272230
Rofaeel Wagdy <rofaeelwagdy@gmail.com>
2025-09-11 00:58:20 +0300

    added CreateTeamScene which is responsible for team creation

---
+++ src/frontend/CreateTeamScene.java
@@ -0,0 +1,73 @@
+package frontend;
+
+import backend.Main;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class CreateTeamScene {
+    private Scene createTeamScene;
+
+    private NavigationBar navBar;
+    private VBox mainContainer;
+
+    private HBox createTeamFormHBox;
+    private VBox rootLayout;
+    private Button createTeamSubmitButton;
+
+
+    private void layoutInitializer(){
+        mainContainer = new VBox();
+        rootLayout = new VBox();
+        createTeamFormHBox = new HBox();
+        navBar = new NavigationBar();
+        createTeamSubmitButton = new Button("Create Team");
+    }
+
+    private void layoutOrganizer(){
+        mainContainer.getChildren().addAll(navBar.getTitleBar(), rootLayout);
+        mainContainer.setSpacing(200);
+
+        rootLayout.getChildren().addAll(createTeamFormHBox, createTeamSubmitButton);
+        rootLayout.setAlignment(Pos.CENTER);
+        rootLayout.setSpacing(30);
+    }
+
+    private void setScene(){
+        createTeamScene = new Scene(mainContainer, Constants.SCENE_WIDTH, Constants.SCENE_HEIGHT);
+    }
+
+    private void showCreateTeamForm(){
+        Label teamNameLabel = new Label("Team Name");
+
+        TextField teamNameTextField = new TextField();
+        teamNameTextField.setPromptText("Team Name");
+
+        createTeamFormHBox.getChildren().addAll(teamNameLabel,  teamNameTextField);
+        createTeamFormHBox.setSpacing(20);
+        createTeamFormHBox.setAlignment(Pos.CENTER);
+
+        createTeamSubmitButton.setOnAction(_ -> {
+            createTeam(teamNameTextField.getText());
+            teamNameTextField.clear();
+        });
+    }
+
+    private void createTeam(String teamName){
+        Main.createTeamFromFrontEnd(teamName);
+        PopUp.display("Team Created", "Team Created Successfully", false);
+    }
+
+    public Scene getScene(){
+//        this.mainWindow = mainWindow;
+        layoutInitializer();
+        layoutOrganizer();
+        setScene();
+        showCreateTeamForm();
+        return createTeamScene;
+    }
+}
