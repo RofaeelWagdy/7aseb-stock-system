f3c9e44f956fd8915d4db3389eda895267a760b2
Rofaeel Wagdy <rofaeelwagdy@gmail.com>
2025-09-10 15:46:50 +0300

    added another rule

--- src/backend/constants/TeamConstants.java
+++ src/backend/constants/TeamConstants.java
@@ -1,7 +1,8 @@
 package backend.constants;
 
 public interface TeamConstants {
-    int AVAILABLE_STOCK_QUANTITY = 100;
+    int AVAILABLE_STOCK_QUANTITY = 120;
     long STOCK_PRICE = 10;
     long BALANCE = 1000;
+    int ALLOWED_NUMBER_SHARES_TO_BUY_FROM_EACH_TEAM = 20;
 }
