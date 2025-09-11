374a36ce9e53510990a477f0b8e201145b27bf7f
Rofaeel Wagdy <rofaeelwagdy@gmail.com>
2025-09-11 15:25:00 +0300

    added logs file name

--- src/backend/constants/FileNames.java
+++ src/backend/constants/FileNames.java
@@ -3,4 +3,5 @@ package backend.constants;
 public interface FileNames {
     String TEAMS_FILENAME = "Teams.txt";
     String SHARES_FILENAME = "Shares.txt";
+    String LOGS_FILENAME = "Logs.txt";
 }
